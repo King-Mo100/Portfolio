aa26125ee57df5856218052f94e80a94035e4b24
moses <mking78911@gmail.com>
2023-06-10 19:35:07 +0100

    BAnner

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,18 +5,14 @@ import { navLinksdata } from '../../constants';
 const Navbar = () => {
   return (
     <div>
-        <div className='w-full h-20 mx-auto flex justify-between items-center'>
-            <img src={logo} alt="logo" className='rounded-full'/>
+        <div className='w-full h-20 mx-auto flex justify-between items-center font-titleFont'>
+            <img src={logo} alt="logo" className='rounded-full'style={{ width: '6.25rem', height: '6.25rem' }} />
         </div>
         <div>
     <ul className='flex items-center gap-10'>
-     {
-        navLinksdata.map((navLink)=>(
-            <lil>
-                {navLink.title}
-            </lil>
-        ))
-     }   
+        {navLinksdata.map(({_id, title, link})=>(
+            <li className='text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300' key={_id}> {title}</li>
+            ))}   
     </ul>        
         </div>
     </div>
