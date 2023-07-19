eb8ec435821485d058ee0a0ef7b7726f3676a6b6
moses <mking78911@gmail.com>
2023-07-20 00:36:46 +0100

    banner, typewriter install and import

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Banner = () => {
   return (
-    <section id='home' className='w-full h-[800px] pb-20 flex items-center border-b-[1px]
-    border-b-black'>Banner</section>
+    <section id='home' className='w-full pt-10 pb-20 flex items-center border-b-[1px] font-titleFont
+    border-b-black'>
+    
+      <div className='w-1/2'>
+        <div className='flex flex-col gap-5'>
+          <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
+          <h1 className='text-6xl font-bold text-white'>Hi, I'm{" "} <span className='text-designColor capitalize'>Moses king</span></h1>
+          <h2 className='text-4x1 font-bold text-white'>a <span>Web Developer</span></h2>
+          <p className='text-base font-bodyFont leading-6 tracking-wide'>
+            I use animation as a third dimension by which to simplify experiences and kuiding thouhj each and every ineraction.  I'm not 
+            adding motion just to spruce things up, but doing it in ways that.
+          </p>
+        </div>
+      </div>
+
+      <div className='w-1/2'></div>
+    </section>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,26 @@ export const content = [
 export const theme = {
   extend: {
     fontFamily: {
-      bodyFont: ["Poppins", "sans-serif"],
-      titleFont: ["Montserrat", "sans-serif"],
+      titleFont: ["Poppins", "sans-serif"],
+      bodyFont: ["Montserrat", "sans-serif"],
     },
     colors: {
       bodyColor: "#212428",
       lightText: "#c4cfde",
-      boxBg: "liner-gradient(145deg, #1e2024, #23272b",
+      boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
       designColor: "#ff014f",
+    },
+    borderColor: {
+      black: "#000000",
     },
     boxShadow: {
       shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
     },
+    textTransform: {
+    capitalize: "capitalize"
+    },
+
+    
   },
 };
 export const plugins = [];
