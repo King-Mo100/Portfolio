9ec1a22cd66f4d8e809d1a34b72ce4923a4e225f
moses <mking78911@gmail.com>
2023-07-20 00:54:05 +0100

    typewriter animation

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Banner = () => {
+  const [text] = useTypewriter({
+    words: ["Professional Coder.", "Web Developer.", "Software Developer."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
   return (
     <section id='home' className='w-full pt-10 pb-20 flex items-center border-b-[1px] font-titleFont
     border-b-black'>
@@ -10,7 +17,7 @@ const Banner = () => {
         <div className='flex flex-col gap-5'>
           <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
           <h1 className='text-6xl font-bold text-white'>Hi, I'm{" "} <span className='text-designColor capitalize'>Moses king</span></h1>
-          <h2 className='text-4x1 font-bold text-white'>a <span>Web Developer</span></h2>
+          <h2 className='text-4x1 font-bold text-white'>a <span>{text}</span></h2>
           <p className='text-base font-bodyFont leading-6 tracking-wide'>
             I use animation as a third dimension by which to simplify experiences and kuiding thouhj each and every ineraction.  I'm not 
             adding motion just to spruce things up, but doing it in ways that.
