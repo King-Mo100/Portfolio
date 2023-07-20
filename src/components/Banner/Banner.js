0fbc9c48e77d12233ed7f755d7e1a80caa4b0af6
moses <mking78911@gmail.com>
2023-07-20 01:22:07 +0100

    h2 typewriter size bug fix

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Banner = () => {
   const [text] = useTypewriter({
@@ -10,31 +10,36 @@ const Banner = () => {
     delaySpeed: 2000,
   });
   return (
-    <section id='home' className='w-full pt-10 pb-20 flex items-center border-b-[1px] font-titleFont
-    border-b-black'>
-    
-      <div className='w-1/2'>
-        <div className='flex flex-col gap-5'>
-          <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
-          <h1 className='text-6xl font-bold text-white'>Hi, I'm{" "} <span className='text-designColor capitalize'>Moses king</span></h1>
-          <h2 className='text-4x1 font-bold text-white'>
+    <section
+      id="home"
+      className="w-full pt-10 pb-20 flex items-center border-b-[1px] font-titleFont border-b-black">
+
+      <div className="w-1/2">
+        <div className="flex flex-col gap-5">
+          <h4 className="text-lg font-normal">WELCOME TO MY WORLD</h4>
+          <h1 className="text-6xl font-bold text-white">
+            Hi, I'm{" "}
+            <span className="text-designColor capitalize">Moses king</span>
+          </h1>
+          <h2 className="text-4xl font-bold text-white">
             a <span>{text}</span>
             <Cursor
-            cursorBlinking="false"
-            cursorStyle="|"
-            cursorColor='#ff014f'
+              cursorBlinking="false"
+              cursorStyle="|"
+              cursorColor="#ff014f"
             />
           </h2>
-          <p className='text-base font-bodyFont leading-6 tracking-wide'>
-            I use animation as a third dimension by which to simplify experiences and kuiding thouhj each and every ineraction.  I'm not 
+          <p className="text-base font-bodyFont leading-6 tracking-wide">
+            I use animation as a third dimension by which to simplify
+            experiences and kuiding thouhj each and every ineraction. I'm not
             adding motion just to spruce things up, but doing it in ways that.
           </p>
         </div>
       </div>
 
-      <div className='w-1/2'></div>
+      <div className="w-1/2"></div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
