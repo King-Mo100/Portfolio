6108583b7e525841cc4474b93371729943d38ca5
moses <mking78911@gmail.com>
2023-09-23 12:03:01 +0100

    removed left banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/Banner/Banner";
-import LeftBanner from "./components/Banner/LeftBanner";
 import Features from "./components/features/features";
 
 function App() {
