f145dc28d3942e5bec1a762d4b81e26873e0151a
moses <mking78911@gmail.com>
2023-09-24 01:38:10 +0100

    features card created and layout folder with title file

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -76,6 +76,7 @@ const Banner = () => {
       </div>
     </div>
   </div>
+  
   )
 }
 
--- src/components/features/features.js
+++ src/components/features/features.js
@@ -1,20 +1,15 @@
 import React from "react";
+import Title from "../layouts/Title";
 
 const Features = () => {
   return (
     <section
       id="features"
-      className="w-full h-[800px] pb-20 flex border-b-[1px] border-b-black"
+      className="w-full h-[800px] py-20 border-b-[1px] border-b-black"
     >
-      <div className="flex flex-col gap-4 font-titleFont">
-        <h3 className="text-sm uppercase font-light text-designColor tracking-wide">
-          Features
-        </h3>
-
-        <h1 className="text-5xl text-gray-300 font-bold capitalize">
-          What I do
-        </h1>
-      </div>
+        <Title title="Features" des="What I Do"/>
+        <div className="w-1/2 px-12 h-80 py-10 rounded-lg sgadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group
+        hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100"></div>
     </section>
   );
 };
