4ac8243c4a9fc3106e797852b679d48c1f1cf7dc
moses <mking78911@gmail.com>
2023-09-24 01:08:24 +0100

    features heading

--- src/components/features/features.js
+++ src/components/features/features.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 const Features = () => {
-    return (
-        <section id="features" className='w-full h-[800px] pb-20 flex border-b-[1px] border-b-black'>
-            Features
-            </section>
-    )
-}
+  return (
+    <section
+      id="features"
+      className="w-full h-[800px] pb-20 flex border-b-[1px] border-b-black"
+    >
+      <div className="flex flex-col gap-4 font-titleFont">
+        <h3 className="text-sm uppercase font-light text-designColor tracking-wide">
+          Features
+        </h3>
 
-export default Features
+        <h1 className="text-5xl text-gray-300 font-bold capitalize">
+          What I do
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
