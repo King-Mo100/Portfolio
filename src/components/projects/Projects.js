3c5c208c26168d7bc8ac6bd7e2266e5488cdcca6
moses <mking78911@gmail.com>
2023-09-26 23:36:48 +0100

    import title and projects section added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Title from '../layouts/Title'
 
 const Projects = () => {
   return (
-    <div>
-        <title />
-    </div>
+    <section id="projects"
+    className="w-full py-20 border-b-[1px] border-b-black">
+        <Title title= "Visit My Portfolio" des="My Projects"/>
+    </section>
   )
 }
 
