ef26bdabde83700bfa756040f7924f1e10c97c8d
moses <mking78911@gmail.com>
2023-09-27 00:16:41 +0100

    projects card title and description added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,13 +7,30 @@ const Projects = () => {
     <section id="projects"
     className="w-full py-20 border-b-[1px] border-b-black">
         
-        <div className='flex justify-center items-center text-center'>
+        <div className='flex justify-center items-center text-center mb-8'>
             <Title 
             title= "Visit My Portfolio" 
             des="My Projects"/>
         </div>
-        <div>
-            <ProjectCards/>
+        <div className='grid grid-cols-3 gap-14'>
+            <ProjectCards
+            Title title="Weather App"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>
+            <ProjectCards
+            Title title="Movie App"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>
+                        <ProjectCards
+            Title title="Bookify"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>
+                        <ProjectCards
+            Title title="Simple Calculator"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>
+                        <ProjectCards
+            Title title="Password Generator"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>
+                        <ProjectCards
+            Title title="Chat Bot"
+            des="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quam."/>            
         </div>
         
     </section>
