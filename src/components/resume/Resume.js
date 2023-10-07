bac91ffe6874193cc600d437e8d938ee4e253722
moses <mking78911@gmail.com>
2023-10-07 21:27:09 +0100

    resume styling added in css component

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -11,18 +11,10 @@ const Resume = () => {
       </div>
       <div>
         <ul className="w-full grid grid-cols-4">
-            <li className="w-full h-20 bg-black bg-opacity-25 text-xl text-gray-300
-            flex justify-center cursor-pointer hover:bg-opacity-40 duration-300 items-center 
-            border-[1px] rounded-none">Education</li>
-            <li className="w-full h-20 bg-black bg-opacity-25 text-xl text-gray-300
-            flex justify-center cursor-pointer hover:bg-opacity-40 duration-300 items-center 
-            border-[1px] rounded-none">Professional Skills</li>
-            <li className="w-full h-20 bg-black bg-opacity-25 text-xl text-gray-300
-            flex justify-center cursor-pointer hover:bg-opacity-40 duration-300 items-center 
-            border-[1px] rounded-none">Experience</li>
-            <li className="w-full h-20 bg-black bg-opacity-25 text-xl text-gray-300
-            flex justify-center cursor-pointer hover:bg-opacity-40 duration-300 items-center 
-            border-[1px] rounded-none">Achievements</li>
+            <li className="resumeLi">Education</li>
+            <li className="resumeLi">Professional Skills</li>
+            <li className="resumeLi">Experience</li>
+            <li className="resumeLi">Achievements</li>
         </ul>
       </div>
     </section>
